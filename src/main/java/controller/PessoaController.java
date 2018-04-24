5d3e415b72ed2820e7674d3addbf15747edea5e8
Maicon Lanzendorf <maicon.lanzendorf@live.com>
2018-04-23 21:22:16 -0300

    aula-23-04-2018
    File Uploads

--- src/main/java/controller/PessoaController.java
+++ src/main/java/controller/PessoaController.java
@@ -1,6 +1,15 @@
 package controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +19,9 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
 
 import model.Pessoa;
 
@@ -34,6 +46,34 @@ public class PessoaController implements Serializable {
 			pessoa.setSexo(i % 2 == 0 ? "M" : "F");
 			pessoas.add(pessoa);
 			pessoa = new Pessoa();
+		}
+	}
+
+	public void upload(FileUploadEvent event) {
+
+		try {
+			// UploadedFile uploadedFile = event.getFile();
+			// File file = new File("C:\\uploads", uploadedFile.getFileName());
+			// OutputStream out = new FileOutputStream(file);
+			// out.write(uploadedFile.getContents());
+			// out.close();
+
+			UploadedFile x = event.getFile();
+			Path t = Files.createTempFile(null, null);
+//			Files.copy(x.getInputstream(), t, StandardCopyOption.REPLACE_EXISTING);
+
+			Path origem = Paths.get(t.toString());
+			Path destino = Paths.get("C:\\uploads\\" + x.getFileName());
+			
+			System.out.println(t.toString());
+
+			Files.copy(origem, destino, StandardCopyOption.REPLACE_EXISTING);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
